8d37714a15a8cdc90ea2e162b1d9c49f32376cbd
Vratislav Jindra <jindra.vratislav@gmail.com>
2026-03-25 07:30:55 +0100

    feat(Compose) Added compose-related dependencies, main activity and setup basic theming

---
+++ app/src/main/java/cz/vratislavjindra/nba/app/ui/theme/NbaShapes.kt
@@ -0,0 +1,14 @@
+package cz.vratislavjindra.nba.app.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.dp
+
+data class NbaShapes(
+    val roundedCornerSmall: Shape = RoundedCornerShape(size = 8.dp),
+    val roundedCornerDefault: Shape = RoundedCornerShape(size = 16.dp),
+    val roundedCornerLarge: Shape = RoundedCornerShape(size = 24.dp),
+)
+
+internal val LocalShapes = staticCompositionLocalOf { NbaShapes() }
